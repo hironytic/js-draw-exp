678a3c85d0f075fa2a424c5c44c132d7a637fbab
Hironori Ichimiya <hiron@hironytic.com>
2013-11-22 22:04:16 +0900

    Initial Commit

---
+++ JSDrawExp/HNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HNAppDelegate.h
+//  JSDrawExp
+//
+//  Created by ichi on 2013/11/22.
+//  Copyright (c) 2013年 Hironytic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSDrawExpTests/JSDrawExpTests.m
@@ -0,0 +1,34 @@
+//
+//  JSDrawExpTests.m
+//  JSDrawExpTests
+//
+//  Created by ichi on 2013/11/22.
+//  Copyright (c) 2013年 Hironytic. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JSDrawExpTests : XCTestCase
+
+@end
+
+@implementation JSDrawExpTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
